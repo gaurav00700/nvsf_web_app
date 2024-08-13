2895c3619110b4548def0271a78043508d84a43d
u29u30 <gaurav.sharma@avl.com>
2024-08-13 15:10:36 +0200

    update api

--- app/api.py
+++ app/api.py
@@ -12,20 +12,20 @@ class Data(BaseModel):
     image_w: int
 
 # Function to capture terminal output
-def capture_output(func):
+def capture_output(func, *args, **kwargs):
     buffer = io.StringIO()
     sys.stdout = buffer
-    func()
+    func(*args, **kwargs)
     sys.stdout = sys.__stdout__
     return buffer.getvalue()
 
-@app.post("/submit")
-def submit_data(data: Data):
-    def generate_output():
-        print(f"Sequence Name: {data.sequence_name}")
-        print(f"Dataset Name: {data.dataset_name}")
-        print(f"Image Height: {data.image_H}")
-        print(f"Image Width: {data.image_w}")
+def generate_output(data: Data):
+    print(f"Sequence Name: {data.sequence_name}")
+    print(f"Dataset Name: {data.dataset_name}")
+    print(f"Image Height: {data.image_H}")
+    print(f"Image Width: {data.image_w}")
 
-    output = capture_output(generate_output)
+@app.post("/submit")
+def submit_data(data):
+    output = capture_output(generate_output(data))
     return {"output": output}
